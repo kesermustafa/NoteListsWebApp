dd22d2e67b621e3834fed7eb7af02cc06e4927d1
Mustafa Keser <keserzade@gmail.com>
2024-04-09 01:56:12 +0300

    update

--- main.js
+++ main.js
@@ -12,4 +12,45 @@ let editElement;
 let editFlag = false; // Düzenleme modunda olup olmadığını belirtir.
 let editID = ""; // Düzenleme yapılan ögenin benzersiz kimliği
 
+// FONKSIYONLAR
 
+const displayAlert = (text, action) => {
+  alert.textContent = text;
+  alert.classList.add(`alert-${action}`);
+
+  setTimeout(() => {
+    alert.textContent = "";
+    alert.classList.remove(`alert-${action}`);
+  }, 2000);
+};
+
+const addItem = (e) => {
+  e.preventDefault();
+  const value = grocery.value; // form icindeki input degeri alindi
+  const id = new Date().getTime().toString(); // benzersiz bi ID olusturduk
+
+  if (value !== "" && !editFlag) {
+    const element = document.createElement("article");
+    let attr = document.createAttribute("data-id");
+    attr.value = id;
+    element.setAttributeNode(attr);
+    element.classList.add("grocery-item");
+
+    element.innerHTML = `
+        <p class="title">${value}</p>
+        <div class="btn-container">
+            <button type="button" class="edit-btn">
+                <i class="fa-solid fa-pen-to-square"></i>
+            </button>
+            <button type="button" class="delete-btn">
+                <i class="fa-solid fa-trash"></i>
+            </button>
+        </div>  
+    `;
+
+    list.appendChild(element);
+    displayAlert("Basariyla eklendi", "success");
+  }
+};
+
+form.addEventListener("submit", addItem);
