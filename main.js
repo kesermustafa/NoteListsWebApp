5b614a372375d736151da0291f3b976e0f7617f1
Mustafa Keser <keserzade@gmail.com>
2024-04-09 18:20:46 +0300

    update

--- main.js
+++ main.js
@@ -14,6 +14,13 @@ let editID = ""; // Düzenleme yapılan ögenin benzersiz kimliği
 
 // FONKSIYONLAR
 
+const setBackToDefault = () => {
+  grocery.value = "";
+  editFlag = false;
+  editID = "";
+  submitBtn.textContent = "Add";
+};
+
 const displayAlert = (text, action) => {
   alert.textContent = text;
   alert.classList.add(`alert-${action}`);
@@ -29,12 +36,20 @@ const deleteItem = (e) => {
   const element = e.currentTarget.parentElement.parentElement;
   const id = element.dataset.id;
   list.removeChild(element);
-  displayAlert("Basariyla silindi", "danger");
+  displayAlert("Deleted successfully", "danger");
+  setBackToDefault();
 };
 
 const editItem = (e) => {
   const element = e.currentTarget.parentElement.parentElement;
   editElement = e.currentTarget.parentElement.previousElementSibling; // kardes elementi sectik
+
+  //tiklanilan elemanin p etiket degeri input a attik
+  grocery.value = editElement.innerText;
+
+  editFlag = true;
+  editID = element.dataset.id;
+  submitBtn.textContent = "Edit";
 };
 
 const addItem = (e) => {
@@ -68,9 +83,48 @@ const addItem = (e) => {
     editBtn.addEventListener("click", editItem);
 
     list.appendChild(element);
-    displayAlert("Basariyla eklendi", "success");
+    displayAlert("Created successfully", "success");
     container.classList.add("show-container");
+
+    // local storage ekleme yap
+    addToLocalStorage(id, value);
+
+    setBackToDefault();
+  } else if (value !== "" && editFlag) {
+    editElement.innerText = value;
+    displayAlert("Updated successfully", "success");
+    setBackToDefault();
   }
 };
 
+const clearItem = () => {
+  const items = document.querySelectorAll(".grocery-item");
+
+  if (items.length > 0) {
+    items.forEach((item) => {
+      list.removeChild(item);
+    });
+  }
+
+  container.classList.remove("show-container");
+  displayAlert("List deleted successfully", "danger");
+  setBackToDefault();
+};
+
+const addToLocalStorage = (id, value) => {
+  const grocery = { id, value };
+
+  let items = getLocalStorage();
+  items.push(grocery);
+
+  localStorage.setItem("list", JSON.stringify(items));
+};
+
+const getLocalStorage = () => {
+  return localStorage.getItem("list")
+    ? JSON.parse(localStorage.getItem("list"))
+    : [];
+};
+
 form.addEventListener("submit", addItem);
+clearBtn.addEventListener("click", clearItem);
